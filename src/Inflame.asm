d90ad8e63e4c1527f72dbfb7d133a8b34894a26e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-12 18:57:06 +0200

    Fix missing brackets

--- src/Inflame.asm
+++ src/Inflame.asm
@@ -29,7 +29,7 @@ proc inject
     cinvoke atoi, dword [eax]
     invoke OpenProcess, PROCESS_VM_WRITE + PROCESS_VM_OPERATION + PROCESS_QUERY_INFORMATION + PROCESS_CREATE_THREAD, FALSE, eax
     mov [processHandle], eax
-    invoke VirtualAllocEx, processHandle, NULL, dllPathLength, MEM_COMMIT + MEM_RESERVE, PAGE_READWRITE
+    invoke VirtualAllocEx, [processHandle], NULL, [dllPathLength], MEM_COMMIT + MEM_RESERVE, PAGE_READWRITE
     mov [allocatedMemory], eax
     invoke WriteProcessMemory, [processHandle], [allocatedMemory], dllPath, [dllPathLength], NULL
     invoke CreateRemoteThread, [processHandle], NULL, 0, [loadLibraryAddress], [allocatedMemory], 0, NULL
