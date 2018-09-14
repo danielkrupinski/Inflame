1df09422fc319115e8821518493f99268954443c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-14 19:57:13 +0200

    Update .text section

--- src/Inflame64.asm
+++ src/Inflame64.asm
@@ -4,6 +4,34 @@ include 'INCLUDE/win64ax.inc'
 
 section '.text' code executable
 
+main:
+    cinvoke __getmainargs, argc, argv, env, 0
+    cmp [argc], 3
+    jne error
+    stdcall inject
+    invoke ExitProcess, 0
+
+error:
+    cinvoke printf, <'Wrong amount of Command Line arguments! Press enter to continue...', 0>
+    cinvoke getchar
+    invoke ExitProcess, 1
+
+proc inject
+    mov esi, [argv]
+    invoke GetFullPathNameA, dword [esi + 4], MAX_PATH, dllPath, 0
+    cinvoke strlen, dllPath
+    inc eax
+    mov [dllPathLength], eax
+    mov esi, [argv]
+    invoke OpenProcess, PROCESS_VM_WRITE + PROCESS_VM_OPERATION + PROCESS_QUERY_INFORMATION + PROCESS_CREATE_THREAD, FALSE, <cinvoke atoi, dword [esi + 8]>
+    mov [processHandle], eax
+    invoke VirtualAllocEx, [processHandle], NULL, [dllPathLength], MEM_COMMIT + MEM_RESERVE, PAGE_READWRITE
+    mov [allocatedMemory], eax
+    invoke WriteProcessMemory, [processHandle], [allocatedMemory], dllPath, [dllPathLength], NULL
+    invoke CreateRemoteThread, [processHandle], NULL, 0, <invoke GetProcAddress, <invoke GetModuleHandleA, <'kernel32.dll', 0>>, <'LoadLibraryA', 0>>, [allocatedMemory], 0, NULL
+    invoke CloseHandle, [processHandle]
+    ret
+endp
 
 section '.data' data readable writable
 
