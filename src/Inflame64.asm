5d9781c244945131703d9c6d4d5866b859465f79
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-14 18:58:06 +0200

    Import functions from kernel32.dll

--- src/Inflame64.asm
+++ src/Inflame64.asm
@@ -6,3 +6,14 @@ section '.idata' data readable import
 
 library kernel32, 'kernel32.dll', \
         msvcrt, 'msvcrt.dll'
+
+import kernel32, \
+       ExitProcess, 'ExitProcess', \
+       GetFullPathNameA, 'GetFullPathNameA', \
+       GetModuleHandleA, 'GetModuleHandleA', \
+       GetProcAddress, 'GetProcAddress', \
+       OpenProcess, 'OpenProcess', \
+       VirtualAllocEx, 'VirtualAllocEx', \
+       WriteProcessMemory, 'WriteProcessMemory', \
+       CreateRemoteThread, 'CreateRemoteThread', \
+       CloseHandle, 'CloseHandle'
