e140b54f44c384ecabd166922d7400e3b0e970d4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-21 16:25:39 +0200

    Revert "Remove structs - manual map injection will be implemented in C++ DLL as it's too complicated for Assembly"
    This reverts commit 906a1a1bef88c6e41f57500d86d2f27bc94ab931.

--- src/Inflame.asm
+++ src/Inflame.asm
@@ -3,6 +3,110 @@ entry main
 
 include 'INCLUDE/win32ax.inc'
 
+struct IMAGE_DATA_DIRECTORY
+    .VirtualAddress dd  ?
+    .isize          dd  ?
+ends
+
+struct IMAGE_OPTIONAL_HEADER32
+    .Magic                         dw  ?
+    .MajorLinkerVersion            db  ?
+    .MinorLinkerVersion            db  ?
+    .SizeOfCode                    dd  ?
+    .SizeOfInitializedData         dd  ?
+    .SizeOfUninitializedData       dd  ?
+    .AddressOfEntryPoint           dd  ?
+    .BaseOfCode                    dd  ?
+    .BaseOfData                    dd  ?
+    .ImageBase                     dd  ?
+    .SectionAlignment              dd  ?
+    .FileAlignment                 dd  ?
+    .MajorOperatingSystemVersion   dw  ?
+    .MinorOperatingSystemVersion   dw  ?
+    .MajorImageVersion             dw  ?
+    .MinorImageVersion             dw  ?
+    .MajorSubsystemVersion         dw  ?
+    .MinorSubsystemVersion         dw  ?
+    .Win32VersionValue             dd  ?
+    .SizeOfImage                   dd  ?
+    .SizeOfHeaders                 dd  ?
+    .CheckSum                      dd  ?
+    .Subsystem                     dw  ?
+    .DllCharacteristics            dw  ?
+    .SizeOfStackReserve            dd  ?
+    .SizeOfStackCommit             dd  ?
+    .SizeOfHeapReserve             dd  ?
+    .SizeOfHeapCommit              dd  ?
+    .LoaderFlags                   dd  ?
+    .NumberOfRvaAndSizes           dd  ?
+    .DataDirectory                 rb (sizeof.IMAGE_DATA_DIRECTORY * 16)
+ends
+
+struct IMAGE_FILE_HEADER
+    .Machine               dw ?
+    .NumberOfSections      dw ?
+    .TimeDateStamp         dd ?
+    .PointerToSymbolTable  dd ?
+    .NumberOfSymbols       dd ?
+    .SizeOfOptionalHeader  dw ?
+    .Characteristics       dw ?
+ends
+
+struct IMAGE_NT_HEADERS
+    .Signature         dd ?
+    .FileHeader        IMAGE_FILE_HEADER
+    .OptionalHeader    IMAGE_OPTIONAL_HEADER32
+ends
+
+struct IMAGE_EXPORT_DIRECTORY
+    .Characteristics       dd  ?
+    .TimeDateStamp         dd  ?
+    .MajorVersion          dw  ?
+    .MinorVersion          dw  ?
+    .nName                 dd  ?
+    .nBase                 dd  ?
+    .NumberOfFunctions     dd  ?
+    .NumberOfNames         dd  ?
+    .AddressOfFunctions    dd  ?
+    .AddressOfNames        dd  ?
+    .AddressOfNameOrdinals dd  ?
+ends
+
+struct IMAGE_DOS_HEADER
+    .e_magic           dw ?
+    .e_cblp            dw ?
+    .e_cp              dw ?
+    .e_crlc            dw ?
+    .e_cparhdr         dw ?
+    .e_minalloc        dw ?
+    .e_maxalloc        dw ?
+    .e_ss              dw ?
+    .e_sp              dw ?
+    .e_csum            dw ?
+    .e_ip              dw ?
+    .e_cs              dw ?
+    .e_lfarlc          dw ?
+    .e_ovno            dw ?
+    .e_res             rw 4
+    .e_oemid           dw ?
+    .e_oeminfo         dw ?
+    .e_res2            rw 10
+    .e_lfanew          dd ?
+ends
+
+struct IMAGE_SECTION_HEADER
+    .Name                 rb 8
+    .VirtualSize          dd ?
+    .VirtualAddress       dd ?
+    .SizeOfRawData        dd ?
+    .OffsetToRawData      dd ?
+    .OffsetToRelocations  dd ?
+    .OffsetToLinenumbers  dd ?
+    .NumberOfRelocations  dw ?
+    .NumberOfLinenumbers  dw ?
+    .Characteristics      dd ?
+ends
+
 section '.text' code executable
 
 main:
@@ -61,7 +165,6 @@ proc injectManualMap
         readBytes dd ?
         processID dd ?
         processHandle dd ?
-        loaderMemory
     endl
 
     mov esi, [argv]
