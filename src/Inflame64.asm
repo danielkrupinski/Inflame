5884615f9b9647aee540d1fed4a08de90686b2ca
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-23 14:23:34 +0200

    Add manualmap label

--- src/Inflame64.asm
+++ src/Inflame64.asm
@@ -30,6 +30,10 @@ loadlibrary:
     stdcall injectLoadLibraryA
     invoke ExitProcess, 0
 
+manualmap:
+    stdcall injectManualMap
+    invoke ExitProcess, 0
+
 error:
     cinvoke printf, <'Wrong amount of Command line arguments! Press enter to continue...', 0>
     cinvoke getchar
