1661d8463ba5e65289ca5ebb348ff712f4252601
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-24 19:37:51 +0200

    Call WaitForSingleObject on thread handle returned from CreateRemoteThread

--- src/Inflame.asm
+++ src/Inflame.asm
@@ -63,6 +63,7 @@ proc injectLoadLibraryA
     lea ebx, [processHandle]
     lea esi, [allocatedMemory]
     invoke CreateRemoteThread, dword [ebx], NULL, 0, <invoke GetProcAddress, <invoke GetModuleHandleA, <'kernel32.dll', 0>>, <'LoadLibraryA', 0>>, dword [esi], 0, NULL
+    invoke WaitForSingleObject, eax, INFINITE
     lea eax, [processHandle]
     invoke CloseHandle, dword [eax]
     ret
